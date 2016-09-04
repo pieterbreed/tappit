92edaef7a94f1645581e6660ec527e1c5d2b9111
Pieter Breed <pieter.breed@gmail.com>
2016-09-04 18:26:31 +0200

    Added travis file and added a few gen tests

--- test/tappit/producer_test.clj
+++ test/tappit/producer_test.clj
@@ -1,7 +1,11 @@
 (ns tappit.producer-test
   (:require [clojure.test :refer :all]
             [tappit.producer :refer :all]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as tcgen]
+            [clojure.test.check.properties :as tcprop]
+            [clojure.test.check.clojure-test :as tct]))
 
 (deftest diag-test
   (let [t (atom {})
@@ -39,3 +43,64 @@
 ;; (no-name-not-ok-test)
 
 
+;; ----------------------------------------
+
+(tct/defspec all-ok-statements-accounted-for-1
+  1000
+  (tcprop/for-all
+   [x tcgen/s-pos-int]
+   (let [output (with-out-str
+                  (with-tap!
+                    (dorun (for [x (range x)]
+                             (ok! x)))))]
+     (and
+      (re-find (re-pattern (str "1.." x)) output)
+      (as-> x $
+        (for [i (range 1 x)]
+          (re-pattern (str "ok " i)))
+        (map #(re-find % output) $)
+        (filter nil? $)
+        (count $)
+        (= 0 $))))))
+
+(tct/defspec inline-diags-accounted-for-1
+  1000
+  (tcprop/for-all
+   [x tcgen/s-pos-int]
+   (let [output (with-out-str
+                  (with-tap!
+                    (dorun (for [x (range x)]
+                             (do
+                               (ok! x)
+                               (diag! (str x)))))))]
+     (and (re-find (re-pattern (str "1.." x)) output)
+          (as-> x $
+            (for [i (range 1 x)]
+              (re-pattern (str "ok " i " # " (dec i))))
+            (map #(re-find % output) $)
+            (filter nil? $)
+            (count $)
+            (= 0 $))))))
+
+(tct/defspec nextline-diags-accounted-for-todos-Allgood-1
+  1000
+  (tcprop/for-all
+   [x tcgen/s-pos-int]
+   (let [output (with-out-str
+                  (with-tap!
+                    (plan-for! x)
+                    (dorun (for [x (range x)]
+                             (do
+                               (ok! x :todo (str "todo" x))
+                               (diag! (str x)))))))]
+     (as-> x $
+       (for [i (range 1 x)]
+         (list (re-pattern (str "[^#]+# TODO todo" (dec i)))
+               (re-pattern (str "# " (dec i)))))
+       (reduce concat (list) $)
+       (concat $ (list (re-pattern (str "1.." x))
+                       (re-pattern "All good")))
+       (map #(re-find % output) $)
+       (filter nil? $)
+       (count $)
+       (= 0 $)))))
